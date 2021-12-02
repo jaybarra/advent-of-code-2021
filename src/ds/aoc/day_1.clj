55c315effe42f68f0a8bc08f47e1bffeff4323d9
Jay Barra <jay.f.barra@gmail.com>
2021-12-01 19:06:26 -0500

    sliding window case

--- src/ds/aoc/day_1.clj
+++ src/ds/aoc/day_1.clj
@@ -1,5 +1,16 @@
 (ns ds.aoc.day-1)
 
 (defn depth-increased-count
+  "Returns the number of instances where the second value is greater than the first."
   [depths]
   (reduce + (map #(if (> %2 %1) 1 0) depths (rest depths))))
+
+(defn sliding-window
+  "Returns the condensed form of a list of values where the values are summed"
+  [depths n]
+  (loop [depths depths
+         new-depths []]
+    (if (> n (count depths))
+      new-depths
+      (recur (rest depths)
+             (conj new-depths (reduce + (take n depths)))))))
--- test/ds/aoc/day_1_test.clj
+++ test/ds/aoc/day_1_test.clj
@@ -1,24 +1,42 @@
 (ns ds.aoc.day-1-test
   (:require
    [clojure.edn :as edn]
-   [clojure.test :refer [deftest is]]
+   [clojure.test :refer [deftest is are]]
    [clojure.java.io :as io]
-   [ds.aoc.day-1 :refer [depth-increased-count]]))
+   [ds.aoc.day-1 :refer [depth-increased-count sliding-window]]))
 
-(def sample-depths [199
-                    200
-                    208
-                    210
-                    200
-                    207
-                    240
-                    269
-                    260
-                    263])
+(def sample-depths
+  [199
+   200
+   208
+   210
+   200
+   207
+   240
+   269
+   260
+   263])
+
+(def depths
+  (with-open [rdr (io/reader (io/resource "day_1/input.txt"))]
+    (reduce #(conj %1 (edn/read-string %2)) [] (line-seq rdr))))
 
 (deftest depth-increased-test
-  (is (= 7 (depth-increased-count sample-depths)))
-  (let [depths
-        (with-open [rdr (io/reader (io/resource "day_1/input.txt"))]
-          (reduce #(conj %1 (edn/read-string %2)) [] (line-seq rdr)))]
-    (is (= 1446 (depth-increased-count depths)))))
+  (are [input expected] (= expected (depth-increased-count input))
+    sample-depths 7
+    depths 1446))
+
+(deftest sliding-window-test
+  (is (= [607
+          618
+          618
+          617
+          647
+          716
+          769
+          792]
+         (sliding-window sample-depths 3)))
+
+  (are [input expected] (= expected (depth-increased-count (sliding-window input 3)))
+    sample-depths 5
+    depths 1486))
